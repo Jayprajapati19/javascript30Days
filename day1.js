19bbbc80fd59aed01fef7ada6964dacae2e1829a
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-13 11:19:29 +0530

    Day 1 Done

---
+++ day1.js
@@ -0,0 +1,82 @@
+// Variables and datatypes 
+
+// task 1 -> Variable declaration using var
+var num1 = 19
+console.log(num1);
+
+// task 2 -> Variable declaration using let 
+let name = "Jay Prajapati"
+console.log(name);
+
+// task 3 -> Constant declaration
+const isLoggedIn = true
+console.log(isLoggedIn);
+
+
+
+
+// task 4 -> Data types
+// Number
+let myNumber = 42;
+console.log(typeof myNumber);
+
+// String
+let myString = "Hello, world!";
+console.log(typeof myString);
+
+// Boolean
+let myBoolean = true;
+console.log(typeof myBoolean);
+
+// Object
+let myObject = { name: "Alice", age: 30 };
+console.log(typeof myObject);
+
+// Array
+let myArray = [1, 2, 3, 4, 5];
+console.log(typeof myArray);
+
+// Function
+let myFunction = function () {
+   return "This is a function";
+};
+console.log(typeof myFunction);
+
+// Undefined
+let myUndefined;
+console.log(typeof myUndefined);
+
+// Null
+let myNull = null;
+console.log(typeof myNull);
+
+// Symbol
+let mySymbol = Symbol('symbol');
+console.log(typeof mySymbol);
+
+// BigInt
+let myBigInt = 12345678901234567890n;
+console.log(typeof myBigInt);
+
+
+// task 5 -> 
+let myvar = 10;
+console.log("Initial value:", myvar);
+
+myvar = 20;
+console.log("New value:", myvar);
+
+
+
+// task 6 -> Understanding Const
+const myConstant = 10;
+console.log(myConstant);
+
+myConstant = 20;
+console.log(myConstant);
+
+
+
+
+
+
