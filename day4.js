ba12ec4b3defdeadb3de6642246e4b82b2e86a46
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-16 08:20:53 +0530

    task 1 and 2 completed

---
+++ day4.js
@@ -0,0 +1,43 @@
+// Day 4 ->  LOOPS
+
+// Activity 1 -> For Loop
+// task 1 -> program to print numbers from 1 to 10 using for loop
+
+for (let i = 1; i <= 10; i++) {
+   // console.log(`${i}`);
+}
+
+// task 2 -> 
+
+for (let j = 1; j <= 10; j++) {
+   console.log(`5 x ${j} = ${5 * j}`);
+}
+
+
+// Activity 2 -> while loop
+
+// task 3 ->  program to calculate the sum of numbers from 1 to 10 using a while loop
+
+
+// task 4 -> program to print numbers from 10 to 1 using whilw loop
+
+// Activity 3 -> Do while loop
+
+// task 5 -> program to print numbers from 1 to 5 using do...whilw loop
+
+// task 6 -> program to calculate the factorial of a number using a do..whilw loop
+
+// Activity 4 -> Nested loops
+// task 7 -> program to print a pattern using nested for loop
+// *
+// **
+// ***
+// ****
+// *****
+
+
+// Activity 5 -> Loop COntrol statements
+
+// task 8 -> Program to print numbers from 1 to 10 , but skip the numbers 5 using the continue statement
+
+// task 9 -> program to print numbers from 1 to 10 , but stop the loop when the numbers is 7 using the break statement
