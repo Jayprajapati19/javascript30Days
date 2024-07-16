17a5991bf254484dbe1829ca8baa9d97f0a72a53
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-16 08:51:27 +0530

    task 6  7 compeleted

--- day4.js
+++ day4.js
@@ -1,39 +1,92 @@
 // Day 4 ->  LOOPS
 
 // Activity 1 -> For Loop
-// task 1 -> program to print numbers from 1 to 10 using for loop
+// task 1 ->  numbers from 1 to 10 using for loop
 
 for (let i = 1; i <= 10; i++) {
-   // console.log(`${i}`);
+   //console.log(`${i}`);
 }
 
 // task 2 -> 
 
 for (let j = 1; j <= 10; j++) {
-   console.log(`5 x ${j} = ${5 * j}`);
+   // console.log(`5 x ${j} = ${5 * j}`);
 }
 
 
 // Activity 2 -> while loop
 
-// task 3 ->  program to calculate the sum of numbers from 1 to 10 using a while loop
+// task 3 -> sum of numbers from 1 to 10 using a while loop
+
+// let sum = 0
+// let i = 1;
+
+// while (i <= 10) {
+//    sum += i;
+//    i++;
+// }
+// console.log(`The sum of numbers from 1 to 10 is: ${sum}`);
 
 
 // task 4 -> program to print numbers from 10 to 1 using whilw loop
+
+// let k = 10;
+
+// while (k >= 1) {
+//    console.log(k);
+//    k--;
+// }
 
 // Activity 3 -> Do while loop
 
 // task 5 -> program to print numbers from 1 to 5 using do...whilw loop
 
+// let j = 1;
+
+// do {
+//    console.log(j);
+//    j++;
+// } while (j <= 5);
+
 // task 6 -> program to calculate the factorial of a number using a do..whilw loop
 
-// Activity 4 -> Nested loops
-// task 7 -> program to print a pattern using nested for loop
-// *
-// **
-// ***
-// ****
-// *****
+// let number = 3
+// let factorial = 1;
+// let i = 1
+
+// do {
+//    factorial *= i;
+//    i++
+// } while (i <= number);
+
+// console.log(`The factorial of ${number} is: ${factorial}`);
+
+/*
+Activity 4 -> Nested loops
+task 7 -> program to print a pattern using nested for loop
+
+*
+**
+***
+****
+***** 
+
+*/
+
+let rows = 5
+
+for (let i = 1; i <= rows; i++) {
+   let pattern = '';
+   for (let j = 1; j <= i; j++) {
+      pattern += '*'
+
+   }
+   console.log(pattern);
+}
+
+
+
+
 
 
 // Activity 5 -> Loop COntrol statements
