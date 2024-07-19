fc3e7c69b7f18c697aa4d2530b5e5ef5d6f4717b
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-19 21:50:07 +0530

    day 7

---
+++ day7.js
@@ -0,0 +1,50 @@
+// day 7 :  Objects
+
+// activity 1: object and creation and access
+
+// task 1: create an object reoresenting a book with properties like title, auther and year and log the object to the console.
+
+
+
+// task 2: access and log title and author properties of the object.
+
+
+
+// activity 2: object method
+
+// task 3: add a method to the book object that return a string with the book's title and author, and log the result of calling this method.
+
+
+
+
+// task 4: add a method to the book object that a parameter and updates the book's propery, then log the upload object.
+
+
+
+// activity 3: nested object
+
+// task 5: create a nested object representing a library  with properties like name and books's and log the library object to the console.
+
+
+
+// task 6: access and log the name of the library and the title of all the books in the library.
+
+
+
+// activity 4: the this keyword
+
+// task 7: add a method to the book object that uses the this keyword to return a string with the book's title and year and log the result of calling this method.
+
+
+
+
+// activity 5: object iteration
+
+// task 8: use a for...in loop to over the properties of the book object and log each property  and it's value.
+
+
+
+// task 9: use object.key and object.value method to log all the keys and values of the book object.
+
+
+
