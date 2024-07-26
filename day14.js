562d068917d40857b57a0617df4bb3dfb75e2600
JAY PRAJAPATI <jayp37679@gmail.com>
2024-07-26 14:21:22 +0530

    add

---
+++ day14.js
@@ -0,0 +1,39 @@
+// DAy 14:Classes
+
+// activity 1: class definition
+
+// task 1: Define a class person with properties name and age, and a method to return  agreeting message. create an instance of the class and log the greeting messgae.
+
+
+// task 2: add a method to the person class that updates the age property and logs the updated age.
+
+
+// activity 2: class Inheritance
+
+// task 3: define a class student that extends the person class. add a property studetId and a method to return the student ID. create an instance of the student class amd log the student ID.
+
+// task 4: override  the greeting method in the studetn class to include the student  ID in the message. log the overridden greeting message.
+
+// activity 3: statics methods and properties
+
+// task 5: add a static method to the person class that returns  a generic greeting message. Call this method without creating an instnace of the class and log the message.
+
+// task 6: add a static property to the studetn class to keep track of the number of students created. increment this property in the constructor and log the total numbers of students.
+
+// activity 4: getters and setters
+
+// task 7: add getter method to hte person class to return the full name (assume a firstname and lastname property) creatr an instance and log the full name using the getter.
+
+// task 8: add setter method to the person class to update  the name property (firstname and lastname) . update the name using the setter and log the updated full name.
+
+// activity 5: pricate fields
+
+// task 9: define a class account with private fields for balance and a method to deposit and withdraw money. Ensure that the balance can only be updated through these methods.
+
+// task 10: create an instance of the account class and test the deposit and withdraw methods, logging the balance after each operation.
+
+
+
+
+
+
